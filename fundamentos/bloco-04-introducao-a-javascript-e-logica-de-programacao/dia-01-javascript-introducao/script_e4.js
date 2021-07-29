9ac43e04b793986e2e1b306d1f0aedc413e511dc
Anderson-Zobel <anderson.zobels@gmail.com>
2021-07-29 13:24:19 -0300

    quarto exercicio

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao/script_e4.js
@@ -0,0 +1,21 @@
+//switch -- case
+
+let aprovacao = "talves";
+
+switch (aprovacao) {
+    case "aprovada":
+        console.log("Parabéns você foi aprovado(a)")
+        break;
+
+    case "lista":
+        console.log("lista de espera")
+        break;
+
+    case "reprovada":
+        console.log("Você foi reprovado(a) ;(")
+        break;
+    
+    default:
+        console.log("Não se aplica")
+
+}
