621e3031190d186fea309fa5d6655c14807b33bc
Anderson-Zobel <anderson.zobels@gmail.com>
2021-07-29 11:38:58 -0300

    segundo exercicio

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao/script_e2.js
@@ -0,0 +1,24 @@
+
+
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// patientId = '50';
+
+// console.log(typeof patientAge)
+// console.log(typeof patientId)
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = 5 + 5 + 8 + 8
+
+console.log(area);
+console.log(perimetro);
+
