207627d3efe20bfaac34fe5b77ab1ca855a11fb0
Anderson-Zobel <anderson.zobels@gmail.com>
2021-07-29 11:22:56 -0300

    adicionado exercicio 1

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao/script.js
@@ -2,6 +2,8 @@ const myName = "Anderson";
 const birthCity = "Porto Alegre";
 let birthYear = 1990;
 
+// birthYear = 2030 //---> funciona por entrar em uma variável
+// birthCity = Recife //--> não funciona por estar em uma const
 
 console.log(myName)
 console.log(birthCity)
