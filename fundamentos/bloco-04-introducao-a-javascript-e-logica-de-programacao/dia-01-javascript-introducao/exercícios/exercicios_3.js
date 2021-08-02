9327d2c6d28cb2f91967de0b1e0906c4f5e7df1d
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-01 21:20:52 -0300

    Finalizado operadores logicos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-introducao/exercícios/exercicios_3.js
@@ -0,0 +1,14 @@
+//Exercício 3
+
+let a = 70
+let b = 120
+let c = 200
+
+if (a > b && a > c) {
+    console.log("'a' é maior que 'b' e 'c'")
+}
+else if (b > a && b > c) {
+    console.log("'b' é maior que 'a' e 'c'")
+} else {
+    console.log("'c' é maior que 'a' e 'b'")
+}
