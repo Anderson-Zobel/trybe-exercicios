f454ab2422afe98b4dcc628c8761d0e3db28c2ba
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-02 18:25:07 -0300

    execicios - conteudo concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-introducao-a-javascript-e-logica-de-programação/exercicios - conteudo/exercicio_3.js
@@ -0,0 +1,14 @@
+// // Que tal praticar um pouco?
+// Pegue cada um dos exercícios da primeira parte das nossas aulas de JavaScript e faça com que todos eles sejam funções de um mesmo arquivo. As variáveis que você define no começo de cada arquivo devem ser removidas e transformadas em parâmetros das funções. Por exemplo:
+
+// Adição
+let a = 5;
+let b = 2;
+
+a + b;
+
+function sum(a, b) {
+    return a + b;
+  }
+
+  console.log(sum.return)
