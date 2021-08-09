97deccd98d02c24e6de98590110597bd3cbd39eb
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-09 20:10:13 -0300

    exercicio finalizado

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-conteúdo/script_3.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-conteúdo/script_3.js
@@ -5,3 +5,25 @@ document.getElementsByTagName('h3')[0].style.backgroundColor = 'red';
 document.getElementsByTagName('h3')[1].style.backgroundColor = 'red';
 document.getElementsByTagName('h3')[2].style.backgroundColor = 'black'; 
 document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+
+// const header = document.getElementById('header-container');
+// header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+// const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+// emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
+
+// const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+// for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+//   emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+// }
+
+// const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+// noEmergencyTasks.style.backgroundColor = 'rgb(249, 219, 94)';
+
+// const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3')
+// for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+//   noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(35, 37, 37)';
+// }
+
+// const footer = document.getElementById('footer-container');
+// footer.style.backgroundColor = 'rgb(0, 53, 51)';
