da6b79b80930f155814694cfb58658e10e14d4bb
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-09 17:51:53 -0300

    adicionado ultimo exercicio fixacao

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-conteúdo/script_3.js
@@ -0,0 +1,7 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.querySelectorAll('.emergency-tasks')[0].style.backgroundColor = 'pink';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'red'; 
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'red';
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black'; 
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
